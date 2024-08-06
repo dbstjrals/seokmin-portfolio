9d793dd0acd5d4eff59ae396565b4c4decfce4bc
dbstjrals <soekmin1998@naver.com>
2024-08-06 20:45:28 +0900

    [Style] Fix colors

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -8,19 +8,36 @@ import { css } from "styled-components";
 //   `;
 // }
 
+const COLORS = {
+  primary: "#7B9ACC",
+  primary50: "#78C6E8",
+  secondary: "#D7C49E",
+  darkBlack: "#263B5D",
+  black: "#171717",
+  white: "#FFFFFF",
+  gray: "rgba(96, 96, 96, 0.6)",
+
+  background: "#FFFFFF",
+};
+
 export const theme = {
   color: {
-    primary: "#7B9ACC",
-    secondary: "#d7c49e",
+    primary: `${COLORS.primary}`,
+    primary50: `${COLORS.primary50}`,
+    secondary: `${COLORS.secondary}`,
+    darkBlack: `${COLORS.darkBlack}`,
+    black: `${COLORS.black}`,
+    gray: `${COLORS.gray}`,
+    white: `${COLORS.white}`,
 
-    background: "#FFFFFF",
+    background: `${COLORS.background}`,
   },
   onHover: {
     highlight: css`
       transition: color 0.2s ease-in-out;
 
       &:hover {
-        color: #d7c49e;
+        color: ${COLORS.secondary};
       }
     `,
     buttonBigger: css`
